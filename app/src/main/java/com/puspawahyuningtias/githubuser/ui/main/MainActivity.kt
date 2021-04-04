49b1304b0c295a871e55899a134f377ca7542dad
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-04-04 23:40:27 +0700

    mengubah MainActivity dan menambahkan user permission internet

---
+++ app/src/main/java/com/puspawahyuningtias/githubuser/ui/main/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.puspawahyuningtias.githubuser.ui.main
+
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.puspawahyuningtias.githubuser.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: MainAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = MainAdapter()
+        adapter.notifyDataSetChanged()
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get(MainViewModel::class.java)
+
+        binding.apply {
+            rvUser.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvUser.setHasFixedSize(true)
+            rvUser.adapter = adapter
+
+            etQuery.setOnKeyListener { v, keyCode, event ->
+                if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    searchUser()
+                    return@setOnKeyListener (true)
+                }
+                return@setOnKeyListener false
+            }
+        }
+        viewModel.getSearchUsers().observe(this,{
+            if(it!=null){
+                adapter.setList(it)
+                showLoading(false)
+            }
+        })
+    }
+
+    private fun searchUser(){
+        binding.apply {
+            val query = etQuery.text.toString()
+            if (query.isEmpty()) return
+            showLoading(true)
+            viewModel.setSearchUsers(query)
+        }
+    }
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
+    }
+}
