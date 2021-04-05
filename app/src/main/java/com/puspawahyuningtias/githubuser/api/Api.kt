a10efa6f71a6aacbe3cefd4ee3123c2627327c57
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-04-05 09:20:24 +0700

    menambahkan endpoint detail, followers dan following

--- app/src/main/java/com/puspawahyuningtias/githubuser/api/Api.kt
+++ app/src/main/java/com/puspawahyuningtias/githubuser/api/Api.kt
@@ -1,9 +1,12 @@
 package com.puspawahyuningtias.githubuser.api
 
+import com.puspawahyuningtias.githubuser.data.model.DetailUserResponse
+import com.puspawahyuningtias.githubuser.data.model.User
 import com.puspawahyuningtias.githubuser.data.model.UserResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface Api {
@@ -12,4 +15,22 @@ interface Api {
     fun getSearchUser(
         @Query("q") query: String
     ): Call<UserResponse>
+    @GET("users/{username}")
+    @Headers("Authorization: token 6101ed46cd8b25ffc75e18b9b121915619ec3c39")
+    fun getUserDetail(
+        @Path("username") username: String
+    ): Call<DetailUserResponse>
+
+    @GET("users/{username}/followers")
+    @Headers("Authorization: token 6101ed46cd8b25ffc75e18b9b121915619ec3c39")
+    fun getFollowers(
+        @Path("username") username: String
+    ): Call<ArrayList<User>>
+
+
+    @GET("users/{username}/following")
+    @Headers("Authorization: token 6101ed46cd8b25ffc75e18b9b121915619ec3c39")
+    fun getFollowing(
+        @Path("username") username: String
+    ): Call<ArrayList<User>>
 }
