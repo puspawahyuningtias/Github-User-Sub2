757f990c3987e39c1e616bf4e0d90afba4c93b74
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-04-06 11:07:34 +0700

    menampilkan following

--- app/src/main/java/com/puspawahyuningtias/githubuser/ui/detail/FollowingFragment.kt
+++ app/src/main/java/com/puspawahyuningtias/githubuser/ui/detail/FollowingFragment.kt
@@ -3,15 +3,59 @@ package com.puspawahyuningtias.githubuser.ui.detail
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.puspawahyuningtias.githubuser.R
 import com.puspawahyuningtias.githubuser.databinding.FragmentFollowBinding
+import com.puspawahyuningtias.githubuser.ui.main.MainAdapter
 
-class FollowingFragment : Fragment(R.layout.fragment_follow){
+class FollowingFragment : Fragment(R.layout.fragment_follow) {
     private var _binding: FragmentFollowBinding? = null
     private val binding get() = _binding!!
+    private lateinit var viewModel: FollowingViewModel
+    private lateinit var adapter: MainAdapter
+    private lateinit var username: String
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val args = arguments
+        username = args?.getString(DetailUserActivity.EXTRA_USERNAME).toString()
+
         _binding = FragmentFollowBinding.bind(view)
+
+
+        adapter = MainAdapter()
+        adapter.notifyDataSetChanged()
+
+        binding.apply {
+            rvUser.setHasFixedSize(true)
+            rvUser.layoutManager = LinearLayoutManager(activity)
+            rvUser.adapter = adapter
+        }
+        showLoading(true)
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get(FollowingViewModel::class.java)
+        viewModel.setListFollowing((username))
+        viewModel.getListFollowing().observe(viewLifecycleOwner, {
+            if (it != null) {
+                adapter.setList(it)
+                showLoading(false)
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            binding.progressBar.visibility = View.VISIBLE
+        } else {
+            binding.progressBar.visibility = View.GONE
+        }
     }
 }
